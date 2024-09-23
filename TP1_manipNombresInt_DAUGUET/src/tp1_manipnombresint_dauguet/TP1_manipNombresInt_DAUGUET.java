eb60d04242c64ea2790738f7215df8884cd4bd9f
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-09-23 18:21:45 +0200

    On fait la partie une du TP1

---
+++ TP1_manipNombresInt_DAUGUET/src/tp1_manipnombresint_dauguet/TP1_manipNombresInt_DAUGUET.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_dauguet;
+
+/**
+ *
+ * @author daugu
+ */
+import java.util.Scanner ;
+public class TP1_manipNombresInt_DAUGUET {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+     
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
