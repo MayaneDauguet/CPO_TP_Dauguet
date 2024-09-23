344f1e9708b787c5e641d13ef48c0103fb7063c7
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-09-23 16:30:59 +0200

    demarrage du tp 0 Exercice 1 fin de l'exercice 1

---
+++ TP0/Exo 1/src/exo/pkg1/Exo1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daugu
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+sc.nextLine();
+System.out.println("Au revoir");
+    }
+    
+}
