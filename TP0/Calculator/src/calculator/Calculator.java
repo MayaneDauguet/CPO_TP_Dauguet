75f5bf0fe2d2ff4cf8fb4b967f219a4e6918911d
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-09-23 17:10:17 +0200

    Exercice 3

--- TP0/Calculator/src/calculator/Calculator.java
+++ TP0/Calculator/src/calculator/Calculator.java
@@ -14,7 +14,12 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
     }
     
 }
