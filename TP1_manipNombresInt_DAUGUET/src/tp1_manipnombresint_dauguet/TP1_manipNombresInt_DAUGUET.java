27cc3f0d84d0fdf9a52be2ebd4330aec97ae4281
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-09-23 18:34:24 +0200

    Fin de la partie 1 du TP1

--- TP1_manipNombresInt_DAUGUET/src/tp1_manipnombresint_dauguet/TP1_manipNombresInt_DAUGUET.java
+++ TP1_manipNombresInt_DAUGUET/src/tp1_manipnombresint_dauguet/TP1_manipNombresInt_DAUGUET.java
@@ -10,15 +10,28 @@ package tp1_manipnombresint_dauguet;
  */
 import java.util.Scanner ;
 public class TP1_manipNombresInt_DAUGUET {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-     
-    }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        // Demande à l'utilisateur de saisir deux entiers
+        System.out.print("Veuillez entrer le premier entier : ");
+        int premierEntier = scanner.nextInt();
+
+        System.out.print("Veuillez entrer le deuxieme entier : ");
+        int deuxiemeEntier = scanner.nextInt();
+
+        int somme = premierEntier + deuxiemeEntier;
+        int difference = premierEntier - deuxiemeEntier;
+        int produit = premierEntier * deuxiemeEntier;
+        int quotient = premierEntier / deuxiemeEntier;
+        int reste = premierEntier % deuxiemeEntier;
+        
+        System.out.println("Vous avez saisi : " + premierEntier + " et " + deuxiemeEntier);
+        System.out.println("La somme des deux nombres est : " + somme);
+        System.out.println("La difference des deux nombres est : " + difference);
+        System.out.println("Le produit des deux nombres est : " + produit);
+        System.out.println("Le quotient de la division de " + premierEntier + " par " + deuxiemeEntier + " est : " + quotient);
+        System.out.println("Le reste de la division de " + premierEntier + " par " + deuxiemeEntier + " est : " + reste);
+
     }
-    
 }
