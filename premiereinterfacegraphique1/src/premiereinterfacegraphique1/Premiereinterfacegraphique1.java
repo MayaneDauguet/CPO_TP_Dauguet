89dd00d01ca9b30d7db6e16f20f6164385cbc05c
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-10-16 08:39:05 +0200

    exo en cours - interface graphique

---
+++ premiereinterfacegraphique1/src/premiereinterfacegraphique1/Premiereinterfacegraphique1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package premiereinterfacegraphique1;
+
+/**
+ *
+ * @author daugu
+ */
+public class Premiereinterfacegraphique1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PremiereFenetre f = new PremiereFenetre();
+        f.show();
+    }
+    
+}
