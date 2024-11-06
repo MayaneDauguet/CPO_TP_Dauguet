895d40555379f75ce8f119379594e30dd2775035
Mayane DAUGUET <mayane.dauguet@epfedu.fr>
2024-11-06 14:40:08 +0100

    TP2 - exo 4

--- TP2_relation_1_DAUGUET/src/tp2_relation_1_dauguet/Personne.java
+++ TP2_relation_1_DAUGUET/src/tp2_relation_1_dauguet/Personne.java
@@ -19,18 +19,32 @@ public class Personne {
         this.nom = nom;
         this.prenom = prenom;
         this.liste_voitures = new Voiture[3]; // Initialisation du tableau pour un maximum de 3 voitures
-        this.nbVoitures = 0; // Initialisation du nombre de voitures à 0
+        this.nbVoitures = 0;
     }
 
     // Méthode pour ajouter une voiture à la liste
-    public void ajouterVoiture(Voiture voiture) {
-        if (nbVoitures < 3) {
-            liste_voitures[nbVoitures] = voiture;
-            voiture.setProprietaire(this); // Définit la personne comme propriétaire de la voiture
-            nbVoitures++;
-        } else {
-            System.out.println("Impossible d'ajouter plus de 3 voitures pour " + prenom + " " + nom);
+    public boolean ajouter_voiture(Voiture voiture_a_ajouter) {
+        // Si la voiture a déjà un propriétaire, on ne l'ajoute pas
+        if (voiture_a_ajouter.proprietaire != null) {
+            System.out.println("La voiture " + voiture_a_ajouter.modele + " est déjà possédée.");
+            return false;
         }
+        
+        // Si la personne possède déjà 3 voitures, on ne peut pas en ajouter
+        if (nbVoitures >= 3) {
+            System.out.println("Impossible d'ajouter une nouvelle voiture, " + prenom + " " + nom + " possède déjà 3 voitures.");
+            return false;
+        }
+        
+        // Ajout de la voiture dans le tableau
+        liste_voitures[nbVoitures] = voiture_a_ajouter; // Ajoute la voiture à la première case libre
+        nbVoitures++; // Incrémente le nombre de voitures possédées
+
+        // Définir la voiture comme ayant cette personne comme propriétaire
+        voiture_a_ajouter.proprietaire = this;
+
+        System.out.println("La voiture " + voiture_a_ajouter.modele + " a été ajoutée à " + prenom + " " + nom);
+        return true;
     }
 
     // Méthode toString pour afficher les informations de la personne
